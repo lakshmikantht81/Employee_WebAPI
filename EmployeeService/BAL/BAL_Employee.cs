d705e55a7a417d91cab492e18d38c2f2b9566b2b
lakshmikantht81 <lakshmikanth.tamarana@outlook.com>
2021-04-12 00:11:44 +0530

    Add project files.

---
+++ EmployeeService/BAL/BAL_Employee.cs
@@ -0,0 +1,72 @@
+﻿using EmployeeService;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeService
+{
+    public class BAL_Employee
+    {
+        public List<EmployeeProfile> GetEmployeeList(int PageNumber, int PageSize)
+        {
+            EmplyeeDBEntities DBContext = new EmplyeeDBEntities();
+            string strQuery = "SELECT* FROM EmployeeProfile ORDER BY EmpNo OFFSET( " + PageNumber + " - 1) * " + PageSize + " ROWS FETCH NEXT " + PageSize + "ROWS ONLY";
+            var employeeList = DBContext.EmployeeProfiles.SqlQuery(strQuery).ToList();
+            return employeeList;
+        }
+
+        public EmployeeProfile AddEditEmploy(EmployeeProfile empProfile)
+        {
+            EmplyeeDBEntities DBContext = new EmplyeeDBEntities();
+            var employ = DBContext.EmployeeProfiles.Where(emp => emp.EmpNo == empProfile.EmpNo).FirstOrDefault();
+            if (employ != null)
+            {
+                employ.Name = empProfile.Name;
+                employ.PhoneNo = empProfile.PhoneNo;
+                employ.Email = empProfile.Email;
+                employ.Gender = empProfile.Gender;
+                employ.Position = empProfile.Position;
+                employ.DOB = empProfile.DOB;
+                employ.Salary = empProfile.Salary;
+
+                DBContext.SaveChanges();
+            }
+            else
+            {
+                var newEmploy = new EmployeeProfile
+                {
+                    EmpNo = empProfile.EmpNo,
+                    Name = empProfile.Name,
+                    PhoneNo = empProfile.PhoneNo,
+                    Email = empProfile.Email,
+                    Gender = empProfile.Gender,
+                    Position = empProfile.Position,
+                    DOB = empProfile.DOB,
+                    Salary = empProfile.Salary,
+                };
+
+                DBContext.EmployeeProfiles.Add(newEmploy);
+                DBContext.SaveChanges();
+            }
+
+            var empDeails = DBContext.EmployeeProfiles.Where(emp => emp.EmpNo == empProfile.EmpNo).FirstOrDefault();
+            return empDeails;
+        }
+
+        public string DeleteEmploy(int EmpNo)
+        {
+            EmplyeeDBEntities DBContext = new EmplyeeDBEntities();
+            var employ = DBContext.EmployeeProfiles.Where(emp => emp.EmpNo == EmpNo).FirstOrDefault();
+            if (employ != null)
+            {
+                DBContext.EmployeeProfiles.Remove(employ);
+                DBContext.SaveChanges();
+            }
+
+            return "Success";
+        }
+    }
+}
---
+++ EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService
+{
+    public class EmployeeController : ApiController
+    {
+        BAL_Employee objEmp = new BAL_Employee();
+
+        [AcceptVerbs("GET", "POST")]
+        [Route("api/Employee/{PageNo}/{PageSize}")]
+        public HttpResponseMessage GET(int PageNo, int PageSize)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, objEmp.GetEmployeeList(PageNo, PageSize));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        [Route("api/Employee")]
+        public HttpResponseMessage PUT(EmployeeProfile empProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, objEmp.AddEditEmploy(empProfile));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        //[AcceptVerbs("GET", "POST")]
+        [Route("api/Employee/{EmpNo}")]
+        public HttpResponseMessage DELETE(int EmpNo)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, objEmp.DeleteEmploy(EmpNo));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+        
+    }
+}
